7c2844820ed44216599e759993e746f71ee172be
aryawirasandi <aryawirasandi7153@gmail.com>
2024-07-23 23:45:56 +0700

    update migration table

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id int AUTO_INCREMENT,
     username VARCHAR(25),
     password VARCHAR(100),
-    token VARCHAR(100),
+    token VARCHAR(100) DEFAULT '',
     role ENUM('staff', 'super') NOT NULL DEFAULT 'staff',
     createdAt DATE,
     updatedAt DATE,
