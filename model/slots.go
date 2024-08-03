e9d185a310a0037496a164a8e32191e3bd15da4a
aryawirasandi <aryawirasandi7153@gmail.com>
2024-08-03 17:30:34 +0700

    adjust query to include only one field

--- model/slots.go
+++ model/slots.go
@@ -1,14 +1,24 @@
 package model
 
 import (
+	"errors"
+
 	"github.com/aryawirasandi/parking-app/entity"
 )
 
-func (m Model) CreateSlot(visitorId string, slotName string) (entity.Slot, error) {
-
-	_, errDb := m.Database.Exec("INSERT INTO slots (visitor_id, slot_name) VALUES (?, ?)", visitorId, slotName)
+func (m Model) CreateSlot(slotName string) (entity.Slot, error) {
 
 	var user entity.Slot
+
+	findFirst := m.Database.QueryRow("SELECT * FROM slots WHERE slot_name = ?", slotName)
+
+	findFirst.Scan(&user.Id, &user.VisitorId, &user.SlotName)
+
+	if user.Id != 0 {
+		return user, errors.New("can't duplicate parking code")
+	}
+
+	_, errDb := m.Database.Exec("INSERT INTO slots (slot_name) VALUES (?)", slotName)
 
 	if errDb != nil {
 		return user, errDb
