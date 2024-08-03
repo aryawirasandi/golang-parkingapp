b060c2f396fab99186dcf05cc5dcb915880f496e
aryawirasandi <aryawirasandi7153@gmail.com>
2024-08-03 17:28:33 +0700

    update migration to set visitor_id as a null

--- db/migrations/000003_create_slots_table.up.sql
+++ db/migrations/000003_create_slots_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE slots (
-    id INT NOT NULL PRIMARY KEY,
-    visitor_id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    visitor_id INT NULL,
     slot_name VARCHAR(50) NOT NULL UNIQUE,
     FOREIGN KEY (visitor_id) REFERENCES visitors (id)
 );
