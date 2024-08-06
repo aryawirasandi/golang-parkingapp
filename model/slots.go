fba9d7097f1828ac1ab1966287a6ba4069c10236
aryawirasandi <aryawirasandi7153@gmail.com>
2024-08-06 21:44:24 +0700

    fix: adjust new model get lists of slot

--- model/slots.go
+++ model/slots.go
@@ -1,10 +1,38 @@
 package model
 
 import (
+	"database/sql"
 	"errors"
+	"log"
 
 	"github.com/aryawirasandi/parking-app/entity"
 )
+
+func (m Model) GetListOfSlot() ([]entity.Slot, error) {
+	slots := []entity.Slot{}
+
+	ul, err := m.Database.Query("SELECT * FROM slots")
+
+	if err != nil {
+		return slots, err
+	}
+
+	for ul.Next() {
+		var u entity.Slot
+		var nv sql.NullString
+
+		if err := ul.Scan(&u.Id, &nv, &u.SlotName); err != nil {
+			log.Fatal(err)
+		} else {
+			u.VisitorId = &nv.String
+		}
+		slots = append(slots, u)
+	}
+
+	defer ul.Close()
+
+	return slots, nil
+}
 
 func (m Model) CreateSlot(slotName string) (entity.Slot, error) {
 
